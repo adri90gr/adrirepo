b67e84612014b04e9a5574fcd5f2de5fe16d88b6
adri90gr <adrian.pajaro@hotmail.com>
2019-05-14 16:52:29 +0200

    modificacion2

---
+++ src/tutorial2/moto.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorial2;
+
+/**
+ *
+ * @author PC
+ */
+public class moto {
+ //esto es una modificacion
+ // esto es otra modificacion
+    
+}
